b905cc90a2ff409eab17b533ba480eaad9ff5026
Goakem <195132463+Goakem@users.noreply.github.com.>
2026-06-22 13:22:21 -0400

    solving commit issues atm
    There is a problem with updating my repository so i am committing my saved progress via github icon built in the Vs code app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RockPaperScissor
-This is an assignmnet project from the odin project foundations and it is the first officail javascript project  to encounter.
+This is an assignment project from the odin project foundations and it is the first officail javascript project  to encounter.
+It is to be noted that I deleted my former repository that has been empty for a long time and I am starting again after going over the Javascript prerequisites in the Odin Project foundation as I was instructed to from the community.
+looking forward to see how much I have improved, thinking back I shouldn't have deleted the former repository and just called this the version 1 lol
+
---
+++ RockPaperScissors.js
@@ -0,0 +1,27 @@
+// Get computerChoice
+
+// Get humanChoice
+
+// Compare human and computer Choice
+    // If humanChoice is 'Rock' and computerChoice is Rock log "It is a tie! you both choose Rock" in the console
+
+    //Else if humanChoice is 'Rock' and computerChoice is Paper log "You lose! Paper Beats Rock" in the console
+
+    // Else if humanChoice is 'Rock' and computerChoice is Scissor log "You win! Rock beats Scissors" in the console 
+
+
+    //If humanChoice is 'Paper' and computerChoice is 'Rock' log "You win! Paper beats Rock" in the console
+
+    //Else if humanChoice is 'Paper' and computerChoice is 'Paper' log "It is a tie, you both choose Paper" in the console
+
+    //Else if humanChoice is 'Paper' and computerChice is 'Scissor' log "You lose!, Scissors beats Paper"
+
+
+    // If humanChoice is 'Scissors' and computerChoice is 'Rock' log "You lose!, Rock beats Scissors"
+
+    // Else if humanChoice is 'Scissors' and computerChoice is 'Paper' log "You win!, Scissors beats Rock."
+
+    // Else if humanChoice is 'Scissors' and computerChoice is 'Scissors' log "It is a tie!, You both choose Scissors"
+
+
+// Print result
