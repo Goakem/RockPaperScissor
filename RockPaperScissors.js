85002ddb14b34da574a263bfdcc72e39988fd31f
Goakem <195132463+Goakem@users.noreply.github.com.>
2026-06-24 05:29:56 -0400

    Editing the parameters to fix the requirement of the game
    I had to remove the forced parameter assignment of the function playRound and also introduced humanSelection and computerSelection and passed them as argument to the playRound function which still works and make the code more correct and meaningful since the other former code is not passing any argument and just working no matter what was passed to it

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -14,7 +14,7 @@ function getComputerChoice () {
 
 // Get humanChoice
 function getHumanChoice () {
-    let humanChoice = prompt('Enter Rock, Paper or Scissors:', '').toLowerCase();
+    let humanChoice = prompt('Enter Rock, Paper or Scissors:', '');
 
     return humanChoice;
 }
@@ -27,31 +27,32 @@ let computerScore = 0;
 //Creating a function to play a round of the game
 function playRound(humanChoice,computerChoice) {
 
-humanChoice = getHumanChoice();
-computerChoice = getComputerChoice();
-
 //  compare and give log the result on the console if any of the condition is met 
-if ((humanChoice === 'rock') && (computerChoice === 'rock')) {
+if ((humanChoice.toLowerCase() === 'rock') && (computerChoice === 'rock')) {
     console.log('It is a tie! You both pick Rock');
-} else if ((humanChoice === 'rock') && (computerChoice === 'paper' )){
+} else if ((humanChoice.toLowerCase() === 'rock') && (computerChoice === 'paper' )){
     console.log('You lose paper beats rock');
-} else if ((humanChoice === 'rock') && (computerChoice === 'scissors')) {
+} else if ((humanChoice.toLowerCase() === 'rock') && (computerChoice === 'scissors')) {
     console.log('You win! rock beats scissors');
-} else if ((humanChoice === 'paper') && (computerChoice === 'rock')) {
+} else if ((humanChoice.toLowerCase() === 'paper') && (computerChoice === 'rock')) {
     console.log('You win! paper beats rock');
-} else if ((humanChoice === 'paper') && (computerChoice === 'paper')) {
+} else if ((humanChoice.toLowerCase() === 'paper') && (computerChoice === 'paper')) {
     console.log('It is a tie! You both pick paper');
-} else if ((humanChoice === 'paper') && (computerChoice === 'scissors')) {
+} else if ((humanChoice.toLowerCase() === 'paper') && (computerChoice === 'scissors')) {
 console.log('You lose! scissors beats paper');
-} else if ((humanChoice === 'scissors') && (computerChoice === 'rock')) {
+} else if ((humanChoice.toLowerCase() === 'scissors') && (computerChoice === 'rock')) {
     console.log('You lose! rock beats scissors');
-} else if ((humanChoice === 'scissors') && (computerChoice === 'paper')) {
+} else if ((humanChoice.toLowerCase() === 'scissors') && (computerChoice === 'paper')) {
     console.log('You win! scissors beats paper');
-} else if ((humanChoice === 'scissors') && (computerChoice === 'scissors')) {
+} else if ((humanChoice.toLowerCase() === 'scissors') && (computerChoice === 'scissors')) {
     console.log('It is a tie! You both pick scissors');
 } else {
     console.log(`Pls pick between 'Rock' 'Paper' or 'Scissors'`);
 }
 
 }
-playRound();
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
